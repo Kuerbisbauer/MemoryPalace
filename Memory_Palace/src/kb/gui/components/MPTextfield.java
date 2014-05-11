ecc2a24d2114e83ca4ef76643501944b8dec3e0b
Martin Szalay <martin.szalay@gmx.at>
2014-05-11 11:11:14 +0200

    MPTextfield - Text verschwindet wenn zum ersten Mal geklickt

---
+++ Memory_Palace/src/kb/gui/components/MPTextfield.java
@@ -0,0 +1,27 @@
+package kb.gui.components;
+
+import javax.swing.JTextField;
+
+public class MPTextfield extends JTextField {
+
+	/**
+	 * Wenn das Textfeld zum ersten Mal angeklickt wurde,
+	 * so wird dieser Wert auf true geändert.
+	 */
+	private boolean clickedState = false;
+	
+	
+	public MPTextfield(String string){
+		super();
+		setText(string);
+	}
+
+
+	public boolean isClickedState() {
+		return clickedState;
+	}
+
+	public void setClickedState(boolean clickedState) {
+		this.clickedState = clickedState;
+	}
+}
--- Memory_Palace/src/kb/gui/newIdea/NewIdea.java
+++ Memory_Palace/src/kb/gui/newIdea/NewIdea.java
@@ -1,6 +1,21 @@
 package kb.gui.newIdea;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JEditorPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import kb.gui.components.MPTextfield;
 
 public class NewIdea extends JDialog {
 	
@@ -29,7 +44,22 @@ public class NewIdea extends JDialog {
 	 * ##############################
 	 */
 	
+	private MPTextfield titleField 	= new MPTextfield("Titel");
 	
+	private JButton	imageButton		= new JButton("Image");
+	private JButton	saveButton		= new JButton("Speichern");
+	private JButton	cancelButton	= new JButton("Abbrechen");
+	
+	private JEditorPane	textField	= new JEditorPane();
+	
+	/*
+	 * ##############################
+	 * Zusätzliche JPanels
+	 * ##############################
+	 */
+	
+	private JPanel	topPanel 	= new JPanel();
+	private JPanel  bottomPanel	= new JPanel();
 	
 	public NewIdea(){
 		this.setModal(true);
@@ -46,6 +76,67 @@ public class NewIdea extends JDialog {
 	 * <li>EditorPane
 	 */
 	private void buildJDialog() {
+		getContentPane().setLayout(new BorderLayout());
 		
+		//Erstellt ein JPanel um Titeltextfeld und Button unterzukriegen
+		topPanel();
+		
+		//Erstellt ein JPanel um die Buttons Speichern und Abbrechen unterzukriegen
+		bottomPanel();
+		
+		getContentPane().add(topPanel, BorderLayout.NORTH);
+		getContentPane().add(textField, BorderLayout.CENTER);
+		getContentPane().add(bottomPanel, BorderLayout.SOUTH);
+		
+		//Actionlistener
+		addActionlistener();
+	}
+
+	/**
+	 * Actionlistener für die Komponenten:
+	 * <li>titleField
+	 * <li>imageButton
+	 * <li>saveButton
+	 * <li>cancelButton
+	 */
+	private void addActionlistener() {
+		titleField.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				
+				//Bedingung: Zum ersten Mal geklickt?
+				if(!titleField.isClickedState()){
+					titleField.setText("");
+					titleField.setClickedState(true);
+				}
+			}
+		});
+	}
+
+	/**
+	 * Ein neues JPanel wird erstellt und mit einem FlowLayout versehen.<br><br>
+	 * Beinhaltet:
+	 * <li>saveButton
+	 * <li>cancelButton
+	 */
+	private void bottomPanel() {
+		bottomPanel.setLayout(new FlowLayout());
+		bottomPanel.add(saveButton);
+		bottomPanel.add(cancelButton);	
+	}
+
+	/**
+	 * Ein neues JPanel wird erstellt und mit einem FlowLayout versehen.<br><br>
+	 * Beinhaltet:
+	 * <li>titleField
+	 * <li>imageButton
+	 */
+	private void topPanel() {
+		topPanel.setLayout(new FlowLayout());
+		
+		titleField.setColumns(25);
+		
+		topPanel.add(titleField);
+		topPanel.add(imageButton);
 	}
 }
